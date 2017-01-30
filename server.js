a731354afa05bdc46411815663220248d65e4bf9
Peter Hunt <naseberry@users.noreply.github.com>
2017-01-30 14:05:21 +0000

    FD-32 Install and configure express for application deployment

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express'),
-      app     = express();
+      app     = express(),
+      port    = process.env.PORT || 5000,
+      path    = require('path');
 
-app.use(express.static(`${__dirname}/_public`));
+app.use('/_public', express.static(__dirname + '/_public'));
 
+// app root
 app.get('/', (req, res) => {
-  res.render('index');
+  res.sendFile(path.join(__dirname + '/_public/index.html'));
 });
 
-app.listen(5000, () => {
-  console.log('Server started on port 5000');
+// requested views
+// using regexp to match request to path
+app.get( /([^\/]+)$/g, (req, res) => {
+  res.sendFile(path.join(__dirname + req.path));
+});
+
+app.listen(port, () => {
+  console.log('Server started on port :' + port);
 });
