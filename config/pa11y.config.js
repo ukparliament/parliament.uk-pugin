9a7e5055db005c84552dcf012c87a6f21fa6c7cb
Peter Hunt <naseberry@users.noreply.github.com>
2017-02-06 13:55:02 +0000

    [WEBSITE-329] Pa11y should test entire directory rather than individual files

--- config/pa11y.config.js
+++ config/pa11y.config.js
@@ -1,11 +1,27 @@
-const filePath = 'file://' + process.cwd();
+const fs = require('fs');
 
-module.exports = {
-  urls: [
-    filePath + '/index.html'
-  ]
+
+exports.standard = 'WCAG2AA'; // set test standards
+exports.ignore = ['warning', 'notice']; // ignore WCAG msg
+// log what's happening to the console
+exports.log = {
+  debug: console.log.bind(console),
+  error: console.error.bind(console),
+  info: console.log.bind(console)
 };
+exports.concurrency = '10'; // set the concurrency here to run more tests in parallel
 
-/*
- @TODO: recursive check for *.html in project
-*/
+
+function getFiles() {
+  var arr = [];
+  var files = fs.readdirSync('./_public/members/');
+  for (var i = 0; i < files.length; i++) {
+    arr.push('file://' + process.cwd() + '/_public/members/' + files[i]);
+  }
+  return arr;
+}
+exports.urls = getFiles();
+
+
+// check module output
+// console.log(module);
