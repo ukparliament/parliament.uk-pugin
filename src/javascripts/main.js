455e4240da44018e56eb7a83b12623d8b788af04
vinternet-pds <vinterj@parliament.uk>
2017-02-13 16:24:42 +0000

    WEBSITE-338

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,43 +1,71 @@
 (function () {
 
-  // JAVASCRIPT AVAILABLE
+  // JAVASCRIPT AVAILABILITY
+
   // switches 'no-js' to 'has-js' class on body
   document.body.className = 'has-js';
 
   // CONTENT SLIDER
-  contentSlider = function (val) {
+  contentSlider = function (ids, cls) {
 
-    // grab elements append and append class to show arrows
-    var elm = document.getElementsByClassName(val);
+    var blk;
+    var elm;
 
-    // To do check for empty array and handle expection
-    var y = elm.length;
-    for (var i = 0; i < y; i++) {
-      var arrow = document.createElement('span');
-      arrow.className = 'open';
-      elm[i].appendChild(arrow);
-      elm[i].nextElementSibling.style.display = 'none';
+    // check for valid id - store variable or handle error
+    if (document.getElementById(ids) !== null) {
+      blk = document.getElementById(ids);
+    } else {
+      console.log('No corresponding id found');
     }
 
-    // toggle content fucntion
+    // check for empty array - store variable or handle error
+    if (blk.getElementsByClassName(cls).length) {
+      elm = blk.getElementsByClassName(cls);
+    } else {
+      console.log('No corresponding classes found');
+    }
+
+    // grab elements array and append class to arrow icons
+    var j = elm.length;
+    for (var i = 0; i < j; i++) {
+      var arw = document.createElement('span');
+      arw.className = 'open';
+      elm[i].appendChild(arw);
+      elm[i].nextElementSibling.style.display = 'none';
+      elm[i].setAttribute('tabindex', (~~[i] + 1));
+    }
+
+    // toggle content function
     function toggleContent(evt) {
-      if (evt.target && evt.target.className == val) {
-        var content = evt.target.nextElementSibling;
-        if (content.style.display == 'none') {
-          content.style.display = 'block';
+      if (evt.target && evt.target.className === cls) {
+        var cnt = evt.target.nextElementSibling;
+        if (cnt.style.display == 'none') {
+          cnt.style.display = 'block';
           evt.target.lastChild.className = 'close';
         } else {
-          content.style.display = 'none';
+          cnt.style.display = 'none';
           evt.target.lastChild.className = 'open';
         }
       }
 
-      evt.preventDefault();
+      evt.stopPropagation();
       return false;
     }
 
-    // click and touch event listeners
-    document.addEventListener('click', toggleContent, false);
-    document.addEventListener('touchend', toggleContent, false);
+    // create event listener for touch and send it as click event
+    blk.addEventListener('touchend', function (evt) {
+      evt.preventDefault();
+      evt.target.click();
+    }, false);
+
+    // create event listener for 'return' keydown and send it as click event
+    blk.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === 13) {
+        evt.target.click();
+      }
+    }, false);
+
+    // create click event listener
+    blk.addEventListener('click', toggleContent, false);
   };
 })();
