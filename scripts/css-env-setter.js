3b7c50fddde7da00ffe3a72c819690dbc16ae899
Jake Mulley <mulleyjb@parliament.uk>
2017-06-27 14:17:31 +0100

    feature(font-fixes): change to correct URL var

--- scripts/css-env-setter.js
+++ scripts/css-env-setter.js
@@ -2,13 +2,13 @@
 const fs =   require('fs'),
 			argv = require('minimist')(process.argv.slice(2));
 			s3BucketURL = 'https://s3-eu-west-1.amazonaws.com/web1devci.static-assets';
-			STATIC_ASSETS_URL = (typeof process.env.STATIC_ASSETS_URL === 'undefined') ? s3BucketURL : process.env.STATIC_ASSETS_URL;
+			STATIC_ASSETS_LOCATION_URL = (typeof process.env.STATIC_ASSETS_LOCATION_URL === 'undefined') ? s3BucketURL : process.env.STATIC_ASSETS_LOCATION_URL;
 
 fs.readFile(argv.file, 'utf8', function (err,data) {
 	if (err) {
 		return console.log(err);
 	}
-	var result = data.replace(new RegExp('<% STATIC_ASSETS_URL %>', 'g'), STATIC_ASSETS_URL);
+	var result = data.replace(new RegExp('<% STATIC_ASSETS_LOCATION_URL %>', 'g'), STATIC_ASSETS_LOCATION_URL);
 	fs.writeFile(argv.file, result, 'utf8', function (err) {
 		 if (err) return console.log(err);
 	});
