8b8691360644f8ea556191c4420031ac9cdb9217
Usman Afzal <afzalu@parliament.uk>
2017-07-05 10:56:15 +0100

    [WEBSITE-1542] Dropdown toggle and refactoring

--- src/javascripts/dropdown.js
+++ src/javascripts/dropdown.js
@@ -1,28 +1,35 @@
 // Opens respective dropdown when clicked
 (function () {
-	var dropdown_toggle,
-		dropdown_content,
+	var
+		dropdown_toggle = document.getElementsByClassName('dropdown__toggle'),
+		dropdown_content = document.getElementsByClassName('dropdown__content'),
 		dropdown_parent;
 
-	// Gather elements
-	dropdown_toggle = document.getElementsByClassName('dropdown__toggle');
+	// Hide dropdowns if JS is on
+	for (var x = 0; x < dropdown_content.length; x++) {
+		dropdown_content[x].classList.add('visually-hidden');
+	}
 
 	// Add listener to dropdowns
-	for (var i = 0; i < dropdown_toggle.length; i++) {
-		dropdown_toggle[i].addEventListener('click', toggleDropdown, false);
+	for (var y = 0; y < dropdown_toggle.length; y++) {
+		dropdown_toggle[y].addEventListener('click', toggleDropdown, false);
 	}
 
 	// Add / Remove hidden class
-	function toggleDropdown(){
+	function toggleDropdown(e){
+		e.preventDefault();
+
 		// Get parent of dropdown
 		dropdown_parent = this.parentElement;
 		dropdown_content = dropdown_parent.querySelector('.dropdown__content');
 
-		//Check dropdown content
-		if (dropdown_content.classList.contains('hidden')) {
-			dropdown_content.classList.remove('hidden');
+		// Check dropdown content
+		if (dropdown_content.classList.contains('visually-hidden')) {
+			this.classList.add('open');
+			dropdown_content.classList.remove('visually-hidden');
 		} else {
-			dropdown_content.classList.add('hidden');
+			this.classList.remove('open');
+			dropdown_content.classList.add('visually-hidden');
 		}
 	}
 
