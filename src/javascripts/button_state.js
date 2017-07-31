6c4552ef727a3f948a4bdbb11099e5b31536cdd9
Peter Hunt <naseberry@users.noreply.github.com>
2017-07-31 15:35:17 +0100

    [WEBDEV-160] button loader script (#254)

--- src/javascripts/button_state.js
+++ src/javascripts/button_state.js
@@ -1,56 +1,16 @@
-// Adds 'loading icon' to button with id of 'btn__loading' when clicked
+// Adds 'loading icon' to button[type="submit"] when clicked
 
 (function () {
-	var btnLoad,
-		input,
-		form,
-		userAgent;
+	var buttons = document.querySelectorAll('button');
 
-	// Grab element with id of btn__loading
-	btnLoad = document.getElementById('btn_loading');
-
-	// Grab element with id of search_box
-	input = document.getElementById('input');
-
-	// Grab element with id of pcSearch
-	form = document.getElementById('postcodeSearch');
-
-	// Get the user agent
-	userAgent = window.navigator.userAgent;
-
-	// Only create the event listener if the element exists within DOM
-	if (form) {
-		// bfcache fix
-		window.addEventListener('pageshow', function (event) {
-			if (event.persisted && btnLoad.classList.contains('btn--loading')) {
-				btnLoad.classList.remove('btn--loading');
-			}
-		});
-		// Event listener for form submit
-		form.addEventListener('submit', frmState, false);
+	for (var i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener('click', btnClick, false);
 	}
 
-
-	function frmState(event) {
-		if (input.checkValidity() == true) {
-
-			if (userAgent.match(/Safari/)) {
-				// Prevent form submission
-				event.preventDefault();
-			}
-
-			// Add btn--loading class to button
-			btnLoad.classList.add('btn--loading');
-
-			// Change aria-pressed to the opposite state
-			btnLoad.setAttribute('aria-pressed', true);
-
-			if (userAgent.match(/Safari/)) {
-				// Resume form submission after 'x' ms
-				setTimeout(function() {
-					form.submit();
-				}, 1000);
-			}
+	function btnClick(e) {
+		if (this.type.toLowerCase() === 'submit') {
+			this.classList.add('btn--loading');
 		}
 	}
+
 })();
