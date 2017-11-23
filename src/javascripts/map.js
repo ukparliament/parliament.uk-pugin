1a2275d0ebc812a91bbf1047f786447cc4f55085
Joshua Waheed <joshua.waheed@gmail.com>
2017-11-23 10:08:53 +0000

    [WEBSITE-2143] Linter amends. (#333)

--- src/javascripts/map.js
+++ src/javascripts/map.js
@@ -67,14 +67,11 @@ UK_Parliament.map = function() {
         accessToken: 'pk.eyJ1IjoiaHVudHAiLCJhIjoiY2l6cXY3NjZpMDAxZzJybzF0aDBvdHRlZCJ9.k1zL5uDY7eUvuSiw3Rdrkw'
       }).addTo(map);
 
-
-
-
       // Addong title to map for screen readers
       L.Control.MapTitle = L.Control.extend({
         onAdd: function(map) {
           var map_title = document.querySelector('data-map-title'),
-              map_title_container = L.DomUtil.create('h3');
+            map_title_container = L.DomUtil.create('h3');
 
           map_title_container.className = 'sr-only';
           map_title_container.innerHTML = map_title;
@@ -86,11 +83,9 @@ UK_Parliament.map = function() {
 
       L.control.mapTitle = function(opts) {
         return new L.Control.Watermark(opts);
-      }
+      };
 
       L.control.mapTitle({ position: 'topLeft' }).addTo(map);
-
-
 
       // Create the GeoJSON layer
       geojson = L.geoJson(data, {
