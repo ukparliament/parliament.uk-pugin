8330e0f496d87b0b92680b4f12cced07c33d22c1
Peter Hunt <naseberry@users.noreply.github.com>
2018-03-12 12:39:46 +0000

    [WEVDEV-489] Updated aria attribute on noscript tags (#404)

--- src/javascripts/noscript_aria_tags.js
+++ src/javascripts/noscript_aria_tags.js
@@ -1,11 +1,9 @@
 // Add aria attribute to <noscript> tags
 
 UK_Parliament.nsAria = function () {
-  var
-    hasJS = document.querySelector('body.has-js'),
-    noScript = document.getElementsByTagName('noscript');
+  var noScript = document.getElementsByTagName('noscript');
 
-  if (hasJS && noScript) {
+  if (noScript) {
     // Loop through collected tags and add aria attribute
     for (var i = 0; i < noScript.length; i++) {
       noScript[i].setAttribute('aria-hidden', 'true');
